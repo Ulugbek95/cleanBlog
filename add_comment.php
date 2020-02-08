5ae5c8913874b0aa6ae1abbfdd7c9a4ee00f110f
Ulugbek95 <60747847+Ulugbek95@users.noreply.github.com>
2020-02-09 01:32:02 +0500

    adfds

--- add_comment.php
+++ add_comment.php
@@ -6,9 +6,9 @@ $useremail = $_POST['email'];
     $userQuery = mysql_query("SELECT `id` FROM users WHERE `name`=\"$username\" AND `email`=\"$useremail\"");
     $userRow = mysql_fetch_assoc($userQuery);
     $user_id=$userRow['id'];
-      $date= date('Y-m-d',time());
-      $comment = $_POST['comment'];
-      $PostId = $_POST['PostId'];
+    $date= date('Y-m-d',time());
+    $comment = $_POST['comment'];
+    $PostId = $_POST['PostId'];
 
 mysql_query("INSERT INTO `post_comments` (`user_id`, `date`, `comment`, `post_id`) VALUES ($user_id, \"$date\", \"$comment\", $PostId);");
 header("location:index.php?PostId=$PostId");
